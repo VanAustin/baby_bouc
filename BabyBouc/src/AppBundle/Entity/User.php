c527f4c13363d5d078b451acc55a9c45a416d39d
Van Austin <llamas.van@gmail.com>
2017-10-10 12:03:51 +0200

    install bundle symfo

---
+++ BabyBouc/src/AppBundle/Entity/User.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ *@ORM\Entity
+ *@ORM\Table(name="user")
+ */
+class User extends BaseUser
+{
+  /**
+   * @ORM\Id
+   * @ORM\Column(type="integer")
+   * @ORM\GeneratedValue(strategy="AUTO")
+   */
+  protected $id;
+
+
+    /**
+     * Get the value of Id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
+
+
+
+ ?>
